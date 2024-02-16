89c6c952a1b3533a2568b8a508d0c2d0d32620eb
tigro0115 <mikhailvelm05@gmail.com>
2024-02-17 00:09:22 +0900

    컴포넌트 비동기 작업

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Main from '../components/section/Main'
 
-export const Channel = () => {
-  return (
-    <div>Channel</div>
-  )
+const Channel = () => {
+    return (
+        <Main>
+            Channel
+        </Main>
+    )
 }
 
 export default Channel
