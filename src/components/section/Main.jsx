1f3dfccbdb561479c2ae0206a755cce8da9ccdd9
tigro0115 <mikhailvelm05@gmail.com>
2024-02-18 18:16:02 +0900

    페이지 SEO 작업

--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 const Main = (props) => {
     return (
-        <>
+        <HelmetProvider>
+            <Helmet
+                titleTemplate="%s | Taehyeong's Youtube"
+                defaultTitle="Taehyeong's Youtube"
+                defer={false}
+            >
+                {props.title && <title>{props.title}</title>}
+                <meta name="description" content={props.description} />
+            </Helmet>
+
             <Header />
             <main id='main' role='main'>
                 {props.children}
             </main>
             <Footer />
-        </>
+        </HelmetProvider>
     )
 }
 
